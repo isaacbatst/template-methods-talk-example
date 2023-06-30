03ccadd8f404b3bce77dc0794dd0ef96ae660918
Isaac Batista <isaacbatst@gmail.com>
2023-06-30 15:00:09 -0300

    código inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Jogo de RPG
+
+- Requisito 1:
+  - Personagens tem:
+    - Atributos: Força, Destreza e Inteligência (1 a 10)
+    - Vida: 100
+    - Podem atacar outro personagem, aplicando 10 de dano - destreza do alvo.
+
+- Requisito 2:
+  - Personagens podem ser de raças diferentes:
+    - Elfo soma a inteligência + destreza no dano
+    - Orc soma 2x a força no dano
---
+++ src/Character.test.ts
@@ -0,0 +1,57 @@
+import {describe, expect, it} from 'vitest';
+import { Character } from './Character';
+import { Elf } from './Elf';
+import { Orc } from './Orc';
+
+describe('Character', () => {
+  it('should have strength, dexterity and intelligence', () => {
+    const isaac = new Character(5, 6, 7);
+    expect(isaac.strength).toBe(5);
+    expect(isaac.dexterity).toBe(6);
+    expect(isaac.intelligence).toBe(7);
+  })
+
+  it('should have 100 life points', () => {
+    const isaac = new Character(5, 6, 7);
+    expect(isaac.life).toBe(100);
+  })
+
+  it('should have 10 base damage', () => {
+    const isaac = new Character(5, 6, 7);
+    const damage = isaac.getBaseDamage();
+    expect(damage).toBe(10);
+  })
+
+  it('should attack dealing base damage minus the target\'s dexterity', () => {
+    const isaac = new Character(5, 6, 7);
+    const goliath = new Character(10, 5, 10);
+    isaac.attack(goliath);
+    expect(goliath.life).toBe(95);
+  })
+
+  it('(elf) should have damage modifier: sum dexterity and intelligence', () => {
+    const elf = new Elf(5, 6, 7);
+    const damage = elf.getDamageModifier();
+    expect(damage).toBe(13);
+  })
+
+  it('(orc) should have damage modifier: 2x strength', () => {
+    const orc = new Orc(5, 6, 7);
+    const damage = orc.getDamageModifier();
+    expect(damage).toBe(10);
+  })
+  
+  it('(elf) should attack dealing 10 plus dexterity plus intelligence minus the target\'s dexterity', () => {
+    const elf = new Elf(5, 6, 7);
+    const goliath = new Character(10, 5, 10);
+    elf.attack(goliath);
+    expect(goliath.life).toBe(82);
+  })
+
+  it('(orc) should attack dealing 10 plus 2x strength minus the target\'s dexterity', () => {
+    const orc = new Orc(5, 6, 7);
+    const goliath = new Character(10, 5, 10);
+    orc.attack(goliath);
+    expect(goliath.life).toBe(85);
+  })
+})
